150471a50f7d034852f4cc080c13df6222d0d4ef
khushi <khushiagarwal7821@gmail.com>
2024-05-27 00:37:35 +0530

    change successfully

--- Backend/Routes/blog.route.js
+++ Backend/Routes/blog.route.js
@@ -1,15 +1,3 @@
-// import express from "express";
-// import blog from "../Controller/blog.controller.js";
-// import { authMiddleware } from "../Middleware/authMiddleware.js";
-// const router = express.Router();
-
-
-// router.post('/blogs',authMiddleware,blog);
-
-
-// export default router;
-// In your blog.route.js file
-
 import express from "express";
 import blog from "../Controller/blog.controller.js";
 import { authMiddleware } from "../Middleware/authMiddleware.js";
--- Backend/index.js
+++ Backend/index.js
@@ -92,3 +92,4 @@ const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+// hyy 
